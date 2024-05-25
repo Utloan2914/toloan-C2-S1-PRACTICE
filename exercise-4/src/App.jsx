1b32fab2db7d3959979c1fe48ba1db99449fd9a0
Utloan2914 <loan.huynh25@student.passerellesnumeriques.org>
2024-05-25 10:46:43 +0700

    exerice_4

--- exercise-4/src/App.jsx
+++ exercise-4/src/App.jsx
@@ -12,6 +12,10 @@ function App() {
         <section className="places-category">
           <ul className="places">
             {/* For each place from AVAILABLE_PLACES, create a Place component */}
+            <Place />
+            {AVAILABLE_PLACES.map((place, index) => (
+              <Place key={index} place={place} />
+            ))}
           </ul>
         </section>
       </main>
